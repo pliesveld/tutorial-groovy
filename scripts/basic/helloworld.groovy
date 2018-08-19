8fcdb8162f7c27a8342df12ac4a7660027bd8428
Patrick Liesveld <pliesveld@users.noreply.github.com>
2018-08-19 14:20:25 -0400

    throttle exectuion

--- scripts/basic/helloworld.groovy
+++ scripts/basic/helloworld.groovy
@@ -2,3 +2,5 @@ System.out.println("Hello World");
 
 
 
+
+
--- scripts/run_scripts.sh
+++ scripts/run_scripts.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+(( TIMESTAMP_ACTION=$(date +%s) ))
+(( TIMESTAMP_CHECK=$(date +%s) ))
 
-inotifywait -r -m -e modify /scripts | 
+inotifywait -r -m -e close_write,moved_to,create,modify -m /scripts | 
 while read path _ file; do 
 	[[ $file =~ ^.*groovy$ ]] && {
-        printf '********%-30s**************\n' "$file"
+
+        (( TIMESTAMP_CHECK=$(date +%s) ))
+
+        if [[ $TIMESTAMP_CHECK -gt $TIMESTAMP_ACTION ]]; then
+            (( TIMESTAMP_CHECK=$(date +%s) ))
+            (( TIMESTAMP_ACTION=$(date +%s) + 3 ))
+        	printf '********%-30s**************\n' "$file"
 		echo '***************************' 
 		groovy $path$file 2> /dev/null
+        fi
 	}
 	[[ $file =~ ^.*js$ ]] && {
         printf '********%-30s**************\n' "$file"
